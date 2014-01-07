20f0ff8e828722ab7e832b58bf9da75de757b234
Jens de Smit <jfdsmit@gmail.com>
2014-01-07 16:04:42 +0100

    Adding simplejson to the requirements

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from distutils.core import setup
-setup(name='jsonfield',
-      version='0.9',
-      packages=['jsonfield'],
-      license='MIT',
-      long_description="A reusable Django field that allows you to store validated JSON in your model.")
+setup(
+    name='jsonfield',
+    version='0.9',
+    packages=['jsonfield'],
+    license='MIT',
+    long_description="A reusable Django field that allows you to store validated JSON in your model.",
+    install_requires=['simplejson'],
+)
